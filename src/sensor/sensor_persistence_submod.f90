68295fe7f51b7472440b878d360e7d0ec732288d
parthiban <parthibansolomon67@gmail.com>
2024-10-06 21:53:02 +0530

    sensor persistence row append logic impl

--- src/sensor/sensor_persistence_submod.f90
+++ src/sensor/sensor_persistence_submod.f90
@@ -1,6 +1,6 @@
         submodule (sensor_persistence_mod) sensor_persistence_submod    
 
-               use sensor_data_mod 
+                use sensor_data_mod 
 
                 implicit none
 
@@ -11,9 +11,17 @@
 
                 module procedure writeRow
                         integer::io_unit
+                        logical::file_exists
                         temperature = row%temperature
+                        inquire(file=get_sensor_data_file(),exist=&
+                        &file_exists)
+                        if (.not. file_exists) then
+                                write(*,"(A)") "sensor data file not&
+&found"
+                        end if
                         open(newunit=io_unit,file=get_sensor_data_file(&
-                        &),status='REPLACE')
+                        &),position='append',status='old',&
+                        &action='write')
                         write(io_unit,nml=emitlist)
                         flush(unit=io_unit)
                         close(unit=io_unit)
