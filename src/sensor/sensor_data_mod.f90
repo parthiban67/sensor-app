ffcca053d056ff130e7d34cc9124593bae410bba
parthiban <parthibansolomon67@gmail.com>
2024-10-12 12:11:11 +0530

    linked list in sensor data mod impl

--- src/sensor/sensor_data_mod.f90
+++ src/sensor/sensor_data_mod.f90
@@ -8,12 +8,139 @@ module sensor_data_mod
 		real::temperature
         end type sensor_data
 
+        type,extends(sensor_data) :: sensor_data_node
+                type(sensor_data_node),pointer::next
+        end type sensor_data_node
+
+        type :: sensor_data_list
+                integer,private::list_count = 0
+                type(sensor_data_node),pointer,private::head => null()
+                contains
+                procedure,pass::length
+                procedure,pass::push
+                procedure,pass::pop
+                procedure,pass::shift
+                procedure,pass::unshift
+                procedure,pass::clear_list
+                final::finalize
+        end type sensor_data_list
+
         contains 
         
+        subroutine finalize(this)
+                type(sensor_data_list),intent(in)::this
+                write(*,*) "called"
+        end subroutine finalize
+
+        subroutine clear_list(this)
+                implicit none
+                class(sensor_data_list),intent(inout)::this
+                type(sensor_data_node),pointer::temp
+                this%list_count = 0
+                do      
+                        if(associated(this%head)) then
+                                temp => this%head
+                                this%head => this%head%next
+                                deallocate(temp)
+                        else
+                                exit
+                        end if
+                end do
+        end subroutine clear_list
+
         character(len=255) function get_sensor_data_file() 
                 character(len=255)::cwd
                 call getcwd(cwd)
                 get_sensor_data_file = trim(cwd) // sensor_data_file
         end function get_sensor_data_file
+
+        integer function length(this)
+                implicit none
+                class(sensor_data_list),intent(in)::this
+                length = this%list_count
+        end function length
+
+        subroutine push(this,temperature)
+                implicit none
+                real,intent(in)::temperature
+                class(sensor_data_list),intent(inout)::this
+                type(sensor_data_node),pointer::temp,new_node
+                allocate(new_node)
+                new_node%temperature = temperature
+                new_node%next => null()
+                if(.not. associated(this%head)) then
+                        this%head => new_node
+                else
+                        temp => this%head
+                        do
+                                if (.not. associated(temp%next)) then
+                                        exit
+                                end if
+                                temp => temp%next
+                        end do
+                        temp%next => new_node
+                end if
+                this%list_count = this%list_count + 1
+        end subroutine push
+
+        function pop(this) result(node)
+                implicit none
+              
+                class(sensor_data_list),intent(inout)::this
+                type(sensor_data_node),pointer::temp,prev
+                !class used instead of type to extract pointer
+                !for sensor_data from sensor_data_node
+                class(sensor_data),pointer::node
+
+                if (this%list_count == 0) then
+                        node => null()
+                else if (this%list_count == 1) then
+                        node => this%head
+                        this%head => null()
+                        this%list_count = this%list_count - 1
+                else
+                        temp => this%head
+                        prev => null()
+                        do
+                                if (.not. associated(temp%next)) then
+                                        exit
+                                end if
+                                prev => temp
+                                temp => temp%next
+                        end do      
+                        prev%next => null()  
+                        this%list_count = this%list_count - 1
+                        node => temp        
+                end if
+        end function pop
+
+        subroutine unshift(this,temperature)
+                implicit none
+                real,intent(in)::temperature
+                class(sensor_data_list),intent(inout)::this
+        end subroutine unshift
+
+        function shift(this) result(node)
+                implicit none
+                class(sensor_data_list),intent(inout)::this
+                class(sensor_data),pointer::node 
+        end function shift
+
+        subroutine read_list(sd_list)
+                implicit none
+                class(sensor_data_list),intent(in)::sd_list
+                class(sensor_data_node),pointer::temp => null()
+
+                temp => sd_list%head
+                        do
+                                if (.not. associated(temp)) then 
+                                        exit
+                                end if
+                                write(*,*) temp%temperature
+                                temp => temp%next
+                        end do
+                return
+
+        end subroutine read_list
 
 end module sensor_data_mod
